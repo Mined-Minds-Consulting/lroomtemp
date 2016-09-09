17f753f1e9241772bdf2556a5378bb9c54af5f4f
Heather Sockney <shockney@atlanticbb.net>
2016-09-09 13:26:05 -0400

    updated database info to current one

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require 'pg'
 
 load "./local_env.rb" if File.exists?("./local_env.rb")
 db_params={
-   host: "lockerroom.c4iif5msrrmw.us-west-2.rds.amazonaws.com",
+   host: "lockerroom.ccar1za80ci4.us-west-2.rds.amazonaws.com",
    port:'5432',
    dbname:'lockerroom',
    user:ENV['user'],
